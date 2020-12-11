f310581741e3e6b424216b0269232065fc2d736b
xiaotiandada <952822399@qq.com>
2020-12-11 11:34:30 +0800

    perf: remove extra code

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -1,19 +1,7 @@
 import { Controller } from 'egg';
-
-import { T } from '../utils/twitter';
-
 export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
-
-    await new Promise((resolve: any) => {
-      T.get('users/search', { q: 'XiaoTianIsMe', count: 2 }, function(error, data) {
-        console.log('error', error);
-        console.log('data', data);
-        resolve(data);
-      });
-    });
-
 
     ctx.body = await ctx.service.test.sayHi('egg');
   }
