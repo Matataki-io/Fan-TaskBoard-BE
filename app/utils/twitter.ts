0729480b821976156e17d76405d87320e8097d14
xiaotiandada <952822399@qq.com>
2020-12-11 19:05:06 +0800

    perf: preface code，remote Twitter api proxy

--- app/utils/twitter.ts
+++ app/utils/twitter.ts
@@ -6,8 +6,8 @@ export const twitterConfig = ({ consumer_key, consumer_secret, access_token_key,
     access_token_secret,
     timeout_ms: 10 * 1000, // optional HTTP request timeout to apply to all requests.
     // strictSSL: true, // optional - requires SSL certificates to be valid.
-    request_options: {
-      proxy: 'http://127.0.0.1:1087',
-    },
+    // request_options: {
+    //   proxy: 'http://127.0.0.1:1087',
+    // },
   };
 };
