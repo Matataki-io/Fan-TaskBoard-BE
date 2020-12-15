7dbe8ada9c6ac117adfad7483a2d59baa7794c32
xiaotiandada <952822399@qq.com>
2020-12-15 21:55:48 +0800

    feat: hide

--- app/schedule/quests_transfer.ts
+++ app/schedule/quests_transfer.ts
@@ -12,7 +12,7 @@ class QuestsTransfer extends Subscription {
 
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    this.ctx.service.quest.receiveTransfer();
+    // this.ctx.service.quest.receiveTransfer();
   }
 }
 
