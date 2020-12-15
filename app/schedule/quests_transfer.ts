d9a776a253e3060b7676190f22b1236a5cd6b66f
xiaotiandada <952822399@qq.com>
2020-12-15 23:41:51 +0800

    feat: modify schedule

--- app/schedule/quests_transfer.ts
+++ app/schedule/quests_transfer.ts
@@ -5,14 +5,14 @@ class QuestsTransfer extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      interval: '60s', // 1 分钟间隔
-      type: 'all', // 指定所有的 worker 都需要执行
+      interval: '3m', // 1 分钟间隔
+      type: 'worker', // 指定所有的 worker 都需要执行
     };
   }
 
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    // this.ctx.service.quest.receiveTransfer();
+    this.ctx.service.quest.receiveTransfer();
   }
 }
 
