7640a4e150cda901f38cb94d1e52f41ad4dc1ced
xiaotiandada <istianlei@qq.com>
2021-01-20 15:54:55 +0800

    feat: set cookie

--- app/controller/cookie.ts
+++ app/controller/cookie.ts
@@ -3,23 +3,36 @@ import * as ms from 'ms';
 
 export default class CookieController extends Controller {
   public async index() {
-    const { ctx } = this;
+    const { ctx, app } = this;
     const { accessToken } = ctx.request.body;
     console.log('accessToken', accessToken);
 
-    ctx.cookies.set('access-token', accessToken, {
-      sameSite: 'none',
-      maxAge: ms('7d'),
-    });
+    try {
+      ctx.cookies.set('access-token', accessToken, {
+        sameSite: 'none',
+        secure: app.config.env === 'prod',
+        maxAge: ms('7d'),
+      });
 
-    ctx.body = {
-      code: 0,
-      data: accessToken,
-    };
+      ctx.body = {
+        code: 0,
+        data: accessToken,
+      };
+    } catch (error) {
+      this.logger.error('set cookie', error);
+      ctx.body = {
+        code: 0,
+        data: accessToken,
+        error: error.toString(),
+      };
+    }
   }
   public async remove() {
-    const ctx = this.ctx;
-    ctx.cookies.set('access-token', null);
+    const { ctx, app } = this;
+    ctx.cookies.set('access-token', null, {
+      sameSite: 'none',
+      secure: app.config.env === 'prod',
+    });
     ctx.status = 204;
   }
 }
