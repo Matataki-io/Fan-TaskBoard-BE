07b4620c42abe05812bbbab66d55bd0aa4d7bdbd
xiaotiandada <istianlei@qq.com>
2021-03-26 16:19:27 +0800

    feat：修改间隔时间和奖励定时器错开

--- app/schedule/quests_end_transfer.ts
+++ app/schedule/quests_end_transfer.ts
@@ -5,7 +5,7 @@ class QuestsTransfer extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      interval: '3m', // 1 分钟间隔
+      interval: '5m', // 1 分钟间隔
       type: 'worker', // 指定所有的 worker 都需要执行
     };
   }
